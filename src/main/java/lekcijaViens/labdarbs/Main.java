914876e722363ef443f26f1935c89f1a80a6372c
Valentina Golubkova <valentina.golubkova@gmail.com>
2022-09-26 21:37:12 +0300

    Initial commit

---
+++ src/main/java/lekcijaViens/labdarbs/Main.java
@@ -0,0 +1,40 @@
+package lekcijaViens.labdarbs;
+
+public class Main {
+    public static void main(String[] args) {
+        //int - integer, satur veselu skaitli
+        int vecums = 33;
+        System.out.println("Mans vecums ir:"+ vecums);
+        System.out.println(vecums);
+        int svars = 70;
+        System.out.println("Vinas svars ir:"+ svars);
+        String sunaVards = "Pookkee";
+        String tekst = "SunaVards ir:";
+        System.out.println(tekst+sunaVards);
+        float apavuIzmers = 44.5F;
+        System.out.println("apavu izmers" + apavuIzmers);
+        boolean isOpen = true;
+        boolean isClosed = false;
+        System.out.println("Vai veikals ir atverts?"+ isOpen);
+        String sunaSkirne = "VAS";
+        char valuta = '$';
+        char dzimums = 'V';
+        System.out.println( "test" + valuta+dzimums);
+        int summa;
+
+        summa = 5+5;
+        System.out.println(summa);
+       int x = 10;
+       int y = -45;
+
+       int rezultats = x+y;
+        System.out.println(rezultats);
+
+
+
+
+
+
+}
+}
+
