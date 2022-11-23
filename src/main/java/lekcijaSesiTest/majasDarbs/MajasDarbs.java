8bea7a41bee5b083cdae49c37752ff561057950d
Valentina Golubkova <valentina.golubkova@gmail.com>
2022-11-23 20:59:31 +0200

    add_Valentina

---
+++ src/main/java/lekcijaSesiTest/majasDarbs/MajasDarbs.java
@@ -0,0 +1,14 @@
+package lekcijaSesiTest.majasDarbs;
+
+public class MajasDarbs {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.add(1,2);
+        calculator.substract(3,2);
+        calculator.multiply(3,3);
+        calculator.divide(4,2);
+        calculator.divide(5,2);
+        calculator.divide(1,3);
+
+    }
+}
